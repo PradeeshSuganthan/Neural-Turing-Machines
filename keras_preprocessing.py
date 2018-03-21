b6a25b48c0e93d21ab6477f2915984b112c0608d
Michael Senoyuit <msenoyui@vt.edu>
2018-03-21 16:58:13 -0400

    fixed  getPlaysAsListOfSequences

--- keras_preprocessing.py
+++ keras_preprocessing.py
@@ -1,5 +1,7 @@
 import glob
 from keras.preprocessing import text
+from keras.utils import to_categorical
+import numpy as np
 
 def import_files(text_files_location="./shakespeare/*.txt"):
     text_file_names = glob.glob(text_files_location)
@@ -10,19 +12,27 @@ def import_files(text_files_location="./shakespeare/*.txt"):
         text_file.close()
     return text_list
     
-def getPlays(lower=True, char_level=False, mode="binary"):
+def getPlaysAsListOfSequences(lower=True, char_level=False):
     plays_files = import_files()
     plays_token = text.Tokenizer(lower=lower, char_level=char_level)
     plays_token.fit_on_texts(plays_files)
     plays_sequenced = plays_token.texts_to_sequences(plays_files)
-    y_plays = []
-    x_plays = []
-    for play in plays_sequenced:
-        y_play = [0] + play
-        x_play = play+ [0]
-        y_plays.append(y_play)
-        x_plays.append(x_play)
+    sequences = list()
+    seq_size=1
+    
+    for play_sequenced in plays_sequenced:
+        for i in range(1, len(play_sequenced)):
+            start = i-seq_size
+            if(start<0):
+                start = 0
+            sequence = play_sequenced[start:i+1]
+            sequences.append(sequence)
+            
+    sequence_array = np.array(sequences)
+    x_plays = sequence_array[:,0:-2]
+    y_plays = sequence_array[:,-1]
     return x_plays, y_plays, plays_token
+
 
 def sequence_to_text(seq, token):
     wordmap = {v: k for k,v in token.word_index.items()}
