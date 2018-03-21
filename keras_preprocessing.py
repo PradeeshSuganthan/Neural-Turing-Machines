4af9eaaba6bbe9574afcbe1dbd18fafcfe0212e0
Michael Senoyuit <msenoyui@vt.edu>
2018-03-21 14:58:54 -0400

    added to preproc

--- keras_preprocessing.py
+++ keras_preprocessing.py
@@ -1,5 +1,5 @@
-from keras.preprocessing import text
 import glob
+from keras.preprocessing import text
 
 def import_files(text_files_location="./shakespeare/*.txt"):
     text_file_names = glob.glob(text_files_location)
@@ -8,7 +8,24 @@ def import_files(text_files_location="./shakespeare/*.txt"):
         text_file = open(text_file_name)
         text_list.append(text_file.read())
         text_file.close()
-     return text_list
+    return text_list
+    
+def getPlays(lower=True, char_level=False, mode="binary"):
+    plays_files = import_files()
+    plays_token = text.Tokenizer(lower=lower, char_level=char_level)
+    plays_token.fit_on_texts(plays_files)
+    plays_sequenced = plays_token.texts_to_sequences(plays_files)
+    y_plays = []
+    x_plays = []
+    for play in plays_sequenced:
+        y_play = [0] + play
+        x_play = play+ [0]
+        y_plays.append(y_play)
+        x_plays.append(x_play)
+    print(len(x_plays))
+    print(len(y_plays))
+    return x_plays, y_plays, plays_token
     
     
-import_files()
+    
+    
--- keras_shakespeare_RNN_MS.py
+++ keras_shakespeare_RNN_MS.py
@@ -1,1 +1,6 @@
+import sys
+sys.path.insert(0, './')
 from keras.preprocessing import text
+import keras_preprocessing as kpp
+
+ x_plays, y_plays, plays_token = kpp.getPlays()
