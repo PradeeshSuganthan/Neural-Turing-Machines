da25756ff9ca569ad434128e73364555662c716b
msenoyuit <msenoyui@vt.edu>
2018-03-25 19:32:21 -0400

    added saving/loading preprocessed data to hdf5 files

--- keras_preprocessing.py
+++ keras_preprocessing.py
@@ -1,4 +1,7 @@
 import glob
+import os
+import re
+import h5py
 from keras.preprocessing import text, sequence
 from keras.utils import to_categorical
 import numpy as np
@@ -13,43 +16,110 @@ def import_files(text_files_location):
     return text_list
 
     
-def getPlaysAsListOfSequences(file_location="./shakespeare/*.txt", lower=True, char_level=False, seq_size=1):
+    
+    
+def write_hdf5(dataDict, DataName, PPDataDir, verbose=False):
+    if verbose:
+        print("Saving processed data to"+PPDataDir+DataName+".hdf5"+" : ", end="", flush=True)
+    if not os.path.isdir(PPDataDir):
+        os.makedirs(PPDataDir)
+    with h5py.File(PPDataDir+DataName+".hdf5", "a") as PPData:
+        for name, data in dataDict.items():
+            PPData[name] = data
+    if verbose:
+        print("Done")
+        
+        
+        
+def read_hdf5(DataName, PPDataDir, verbose=False):
+    dataDict = dict()
+    if verbose:
+        print("Loading processed data form"+PPDataDir+DataName+".hdf5"+" : ", end="", flush=True)
+    with h5py.File(PPDataDir+DataName+".hdf5", "r") as PPData:
+        for name, data in PPData.items():
+            dataDict[name] = data[:]
+    if verbose:
+        print("Done")
+    return dataDict
+
+    
+    
+def getPlaysAsListOfSequences(file_location="./shakespeare/*.txt", PPDataDir = "./processed_data/", verbose=False, lower=True, char_level=False, seq_size=1):
+    savedDataName = re.sub(r'\W+', '', file_location)+'_'+str(int(char_level))
+    if verbose:
+        print("Loading and Tokenizing Data : ", end="", flush=True)
     plays_files = import_files(file_location)
     plays_token = text.Tokenizer(lower=lower, char_level=char_level)
     plays_token.fit_on_texts(plays_files)
-    plays_sequenced = plays_token.texts_to_sequences(plays_files)
-    sequences = list()
-    
-    for play_sequenced in plays_sequenced:
-        for i in range(1, len(play_sequenced)):
-            start = i-seq_size
-            if(start<0):
-                start = 0
-            seq = play_sequenced[start:i+1]
-            sequences.append(seq)
+    if verbose:
+        print("Done")
+        
+        
+    if(os.path.exists(PPDataDir +savedDataName+".hdf5")):
+        if verbose:
+            print("Found Preprocessed Data")
+            print("Loading Preprocessed Data: ",end="", flush=True)
+        dataDict = read_hdf5(savedDataName, PPDataDir, verbose=verbose)
+        x_plays = dataDict['x_plays']
+        y_plays = dataDict['y_plays']
+        if verbose:
+            print("Done")
             
-    sequence_array = sequence.pad_sequences(sequences)
-    x_plays = sequence_array[:,0:-1]
-    y_plays = sequence_array[:,-1]
+            
+    else:
+        if verbose:
+            print("No Preprocessed Data Found")
+        plays_sequenced = plays_token.texts_to_sequences(plays_files)
+        sequences = list()
+        if verbose:
+            print("Splitting into seq: ", end="", flush=True)
+        for play_sequenced in plays_sequenced:
+            for i in range(1, len(play_sequenced)):
+                start = i-seq_size
+                if(start<0):
+                    start = 0
+                seq = play_sequenced[start:i+1]
+                sequences.append(seq)
+                
+                
+        if verbose:
+            print("Done")  
+            print("Splitting x and y: ", end="", flush=True)
+        sequence_array = sequence.pad_sequences(sequences)
+        x_plays = sequence_array[:,0:-1]
+        y_plays = sequence_array[:,-1]
+        if verbose:
+            print("Done")
+            
+            
+        dataDict = {'x_plays': x_plays, 'y_plays':y_plays}
+        write_hdf5(dataDict, savedDataName, PPDataDir, verbose=verbose)
+        if verbose:
+            print("Done")
+            
     return x_plays, y_plays, plays_token
 
 
-def sequence_to_text(seq, token):
+def sequence_to_text(seq, token, char_level=False):
     wordmap = {v: k for k,v in token.word_index.items()}
     script = ""
     for word in seq:
-        script += wordmap[word] + " "
+        script += wordmap[word]
+        if not char_level: script += "1"
     return script
 
 
-def genSequence(model, token, sample_len = 50, seq_size=50):
+def genSequence(model, token, sample_len = 50, seq_size=50, char_level=False, verbose=False):
+    if verbose:
+        print("Generating text: ", end="", flush=True)
     seed_text = np.zeros([1,seq_size])
     prediction = ""
     for  _  in range(sample_len):
         predict = model.predict_classes(seed_text, verbose = 0)
-        text = sequence_to_text([predict[0]], token)
+        text = sequence_to_text([predict[0]], token, char_level)
         prediction += text
         seed_text = np.append(seed_text[:,1:],[predict], axis=1)
-
+    if verbose:
+        print("Done")
     print("-" + 'Generated sequence' + '-')
     return prediction
--- keras_shakespeare_RNN_MS.py
+++ keras_shakespeare_RNN_MS.py
@@ -4,21 +4,26 @@ sys.path.insert(0, './')
 from keras.preprocessing import text
 from keras.models import Sequential
 from keras.utils import to_categorical
-from keras.layers import Dense, Dropout, LSTM, Embedding
+from keras.layers import Dense, Dropout, LSTM, Embedding, GRU
 import keras_preprocessing as kpp
 
 
 seq_size = 50
 sample_length =500
+char_level=True
+batch_size = 256
+epochs = 1
+verbose = 1
 
-x_plays, y_plays, plays_token = kpp.getPlaysAsListOfSequences(seq_size=seq_size)
-print('got data')
+x_plays, y_plays, plays_token = kpp.getPlaysAsListOfSequences(char_level=char_level,seq_size=seq_size, verbose=verbose)
+
 vocab_size = len(plays_token.word_index) +1
 
-
+if verbose:
+    print('Building Model')
 model = Sequential()
-model.add(Embedding(vocab_size, 50, mask_zero=True, input_length=seq_size))
-model.add(LSTM(50))
+model.add(Embedding(vocab_size, 500, mask_zero=True, input_length=seq_size))
+model.add(LSTM(500))
 model.add(Dense(vocab_size, activation='softmax'))
 
 print(model.summary())
@@ -26,7 +31,7 @@ print(model.summary())
 model.compile(loss='sparse_categorical_crossentropy',  optimizer='adam', metrics=['sparse_categorical_accuracy'])
 
 
-history = model.fit(x_plays[:10000,:], y_plays[:10000], verbose=1, epochs=1, batch_size=32)
+history = model.fit(x_plays[:1000,:], y_plays[:1000], verbose=1, epochs=epochs, batch_size=batch_size)
 
 
-print(kpp.genSequence(model, plays_token, sample_length, seq_size))
+print(kpp.genSequence(model, plays_token, sample_length, seq_size, char_level, verbose=verbose))
