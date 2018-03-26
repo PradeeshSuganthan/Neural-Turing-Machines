3d04e25475d1bc9657192696110362c83312a354
PradeeshSuganthan <pradeesh.suganthan@gmail.com>
2018-03-26 10:13:25 -0700

    changing model

--- keras_shakespeare_rnn_ps.py
+++ keras_shakespeare_rnn_ps.py
@@ -2,11 +2,11 @@ import numpy as np
 import keras_preprocessing as kpp
 
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Embedding, GRU
+from keras.layers import Dense, LSTM, Embedding, GRU, Dropout
 
 #two words in, one word out sequence
 
-x_plays, y_plays, plays_token = kpp.getPlaysAsListOfSequences(seq_size=10)
+x_plays, y_plays, plays_token = kpp.getPlaysAsListOfSequences(seq_size=50, char_level=True, verbose=1)
 
 vocabulary = len(plays_token.word_index) + 1
 
@@ -14,7 +14,8 @@ vocabulary = len(plays_token.word_index) + 1
 model = Sequential()
 
 model.add(Embedding(vocabulary, 500))
-model.add(GRU(500, return_sequences=True))
+model.add(LSTM(500, return_sequences=True))
+model.add(Dropout(0.25))
 model.add(LSTM(500))
 model.add(Dense(vocabulary, activation = 'softmax'))
 
@@ -22,6 +23,6 @@ print(model.summary())
 
 model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['sparse_categorical_accuracy'])
 
-model.fit(x_plays,y_plays,epochs=3,verbose = 1, batch_size = 200)
+model.fit(x_plays,y_plays,epochs=1,verbose = 1, batch_size = 128)
 
-print(kpp.genSequence(model, plays_token, seq_size=10))
+print(kpp.genSequence(model, plays_token, seq_size=50, char_level=True, verbose=1))
